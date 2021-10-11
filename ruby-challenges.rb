6afe8f3ebafda3cd00e58138cd01c6fdaf6029f0
hackcoder77 <hackcoder77@gmail.com>
2021-10-11 09:14:58 -0700

    week-5-assessment-ato

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -7,8 +7,16 @@ beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
 
 letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
+
+result = beverages_array.select {|item| item.include?("o")}
+p result
+
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
+
+result = beverages_array.select {|item| item.include?("t")}
+p result
+
 
 
 # -------------------2) Create a method that takes in an array of numbers and returns the sum of the numbers. Use the test variables provided.
@@ -16,9 +24,15 @@ letter_t = 't'
 nums_array1 = [42, 7, 27]
 # Expected output: 76
 
+total = nums_array1.sum
+p total
+
+
 nums_array2 = [25, 17, 47, 11]
 # Expected output: 100
 
+total = nums_array2.sum
+p total
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
@@ -26,9 +40,38 @@ nums_array2 = [25, 17, 47, 11]
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
 
-
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(10) => 20
 # Expected output example: my_bike.brake(25) => 0
+
+class Bike
+    def initialize(wheels, model, current_speed)
+        @current_speed = 0
+        @wheels = 2
+        @model = 'BMX'
+    end
+
+    def pedal_faster
+         @current_speed += 10
+     end 
+
+    def brake
+         @current_speed -= 10
+         @current_speed.clamp(0, 50)
+
+     end
+
+    def bike_info
+        "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} MPH." 
+    end
+end
+
+flying = Bike.new(2 ,'BMX' ,10)
+
+puts flying.bike_info
+  
+
+
+
